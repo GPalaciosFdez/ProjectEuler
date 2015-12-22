6781da49759e348b453395fcdf1ca1041ed10890
GPalaciosFdez <g.palaciosfdez@gmail.com>
2015-12-22 14:55:01 +0000

    Problem 8

---
+++ Problem8.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Problem8{
+    
+    public static void main (String[] args) {
+        String input = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+        ArrayList<Integer> arrIn = new ArrayList<Integer>();
+        arrIn = toArrayList(input);
+        long product = maxProduct(arrIn, 13);
+        System.out.println(product);
+    }
+    
+    //Convert the input String into an ArrayList
+    public static ArrayList<Integer> toArrayList(String str){
+        String[] str2 = str.split("");
+        ArrayList<Integer> str3 = new ArrayList<Integer>();
+        for(int i = 1; i<str2.length; i++){
+            str3.add(Integer.parseInt(str2[i]));
+        }
+        return str3;
+    }
+    
+    
+    public static long maxProduct(ArrayList<Integer> in, int adj){
+        long maxProduct = 0;
+        //Should check that the input ArrayList has at least 13 elements
+        for(int i = 0; i<in.size()-adj; i++){
+            long result = 1;
+            for(int j = i; j < i+adj; j++){
+                if(in.get(j).equals(0)){
+                    i+=adj-1;
+                    result = 1;
+                    break;
+                }
+                else{
+                    result *= in.get(j);
+                }
+            }
+            if(result>maxProduct){
+                maxProduct = result;
+            }
+        }
+        return maxProduct;
+    }
+    
+}
