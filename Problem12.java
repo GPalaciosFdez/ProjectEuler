1524e534936a8d8462ba429e47c2406d0f199e67
Guillermo Palacios Fernández <g.palaciosfdez@gmail.com>
2016-03-21 00:08:36 +0100

    Problem 12

---
+++ Problem12.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Problem12{
+
+  public static void main(String[] args) {
+    int divisors=1;
+    int triangle=1;
+    int next=2;
+    while(divisors<500){
+      divisors = numberOfDivisors(triangle);
+      triangle+=next;
+      next++;
+    }
+    next--;
+    System.out.println(triangle-next);
+  }
+
+  public static int numberOfDivisors(int n){
+    int num=1;
+    for (int i=2; i*i<=n; i++){
+      if(n%i==0){
+        num++;
+      }
+    }
+    return num*2;
+  }
+}
